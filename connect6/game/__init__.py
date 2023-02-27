cd931c82035808cbcf44b22e58f06649e1ed817f
Alexander Ploshkin <alexploshkin@gmail.com>
2023-02-27 22:06:16 +0400

    update linters and pre-commit hooks

--- connect6/game/__init__.py
+++ connect6/game/__init__.py
@@ -1,3 +1,3 @@
-from connect6.game.engine import GameEngine  # noqa: F401
-from connect6.game.player import Player  # noqa: F401
-from connect6.game.turn_data import TurnData  # noqa: F401
+from connect6.game.engine import GameEngine
+from connect6.game.player import Player
+from connect6.game.turn_data import TurnData
